3f4303a811bfb2262b88ba90a0027e937951d5b4
Mr. M <judson.murray@nbc.ca>
2022-04-26 09:14:51 -0300

    Initial Version

---
+++ dbpInterfaceExample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dbpInterfaceExample.FrontEnd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testing.....");
+        }
+    }
+}
